2fb6776ba4e531662c8bcc5c89b3e56e2453ddf9
rushalgawali <100396102+rushalgawali@users.noreply.github.com>
2026-01-23 23:03:35 -0500

    Add refined timeline process flow component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import CapabilitiesSection from "@/components/CapabilitiesSection";
 import CNCVMCShowcase from "@/components/CNCVMCShowcase";
 import Reveal from "@/components/Reveal";
 import Clients from "@/components/Clients";
-import ProcessTimeline from "@/components/ProcessTimeline";
+import ProcessTimeline from "@/components/TimelineProcessFlow";
 
 export default function Home() {
   return (
